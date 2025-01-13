1c82b35e09dbd3c11a95c7e4054b8e53ffd49c1a
Brucedevnairobi <bruce14simiyu@gmail.com>
2025-01-13 21:56:33 +0300

    dusting the project

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-       <h1 className="bg-red-400 text-center text-4xl">Footer Section</h1>
+       <h1 className="text-2xl">Footer Section</h1>
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,21 @@
 const Hero = () => {
   return (
-   <section></section>
+   <section id="home" className="w-full flex-col xl:flex-row justify-center
+    min-h-screen gap-10 max-container ">
+      {/* left content*/}
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full 
+      max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival</span><br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14
+        sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation
+           for your active life</p>
+      </div>
+   </section>
   )
 }
 
